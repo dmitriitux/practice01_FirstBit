3c3ab17b85bd27bca2ac82b85be638ee3d6cbd42
Nikita <hipstarkippo@gmail.com>
2022-02-07 10:43:52 +0300

    1.1 corr03

--- 1.1 authorization/backend.php
+++ 1.1 authorization/backend.php
@@ -3,18 +3,20 @@
 
 define('UNAME', 'SWAAROG');
 define('PSWRD', '1234');
-$username = isset($_POST['username']) ? $_POST['username'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
+$username = $_POST['username'] ?? false;
+$password = $_POST['password'] ?? false;
 
-if ((!empty($username)) && (!empty($password))) {
+if (is_string($username) && is_string($password)) {
   if ((UNAME == $username) && (PSWRD == $password)) {
     echo 'You are logged in as Vasily Petrovich';
   } elseif (UNAME != $username) {
     echo 'Login is not right!';
-    $error = 'Authorisation Error';
+    $errorUser = 'Authorisation Error';
+  } elseif (strlen($password) < 4) {
+	 echo 'Minimum 4 characters in the password.';
   } else {
     echo 'Password is not right!';
-    $error = 'Authorisation Error';
+    $errorPsw = 'Authorisation Error';
   }
 } else {
   echo 'Fill in all the fields!';

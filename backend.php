de3682de1304769dcb85d08ade8a73565e71e537
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-02-10 16:22:15 +0300

    переписал php

--- backend.php
+++ backend.php
@@ -1,26 +1,51 @@
 <?php
 
-
+// правильный логин и пароль
 define('UNAME', 'SWAAROG');
 define('PSWRD', '1234');
-$username = $_POST['username'] ?? false;
-$password = $_POST['password'] ?? false;
 
-if (is_string($username) && is_string($password)) {
-  if ((UNAME == $username) && (PSWRD == $password)) {
-    echo 'You are logged in as Vasily Petrovich';
-  } elseif (UNAME != $username) {
-    echo 'Login is not right!';
-    $errorUser = 'Authorisation Error';
-  } elseif (strlen($password) < 4) {
-	 echo 'Minimum 4 characters in the password.';
-  } else {
-    echo 'Password is not right!';
-    $errorPsw = 'Authorisation Error';
-  }
-} else {
-  echo 'Fill in all the fields!';
+/**
+ * Функция рендера ответа
+ *
+ * @param $output
+ *
+ * @return void
+ */
+function output($result = false, $messages = [], $data = [])
+{
+	header('Content-Type: application/json');
+	$output_data = [
+		'result'   => $result,
+		'data'     => $data,
+		'messages' => $messages,
+	];
+	echo json_encode($output_data);
+	die();
 }
 
+// логин и пароль из post запроса
+$username = (string) $_POST['username'] ?? '';
+$password = (string) $_POST['password'] ?? '';
 
-?>
+// массив для отдачи для ответа
+
+
+// проверяем на пустоту поля
+if (empty($username) || empty($password))
+{
+	output(false, ['Fill in all the fields!']);
+}
+
+// пароль должен быть не меньше 4 символов
+if (strlen($password) < 4)
+{
+	output(false, ['Minimum 4 characters in the password.']);
+}
+
+// проверка на пользователя
+if ((UNAME !== $username) || (PSWRD !== $password))
+{
+	output(false, ['Login or password incorrect.']);
+}
+
+output(true, ['You are logged in as Vasily Petrovich']);
